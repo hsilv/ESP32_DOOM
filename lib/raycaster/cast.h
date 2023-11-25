740a9502a9edf2f9bb4012ab154641b0fd47702e
hsilv <hm.sebastiansilva@gmail.com>
2023-11-25 17:11:57 -0600

    feat: (Welcome and Walls) hit walls and welcome

--- lib/raycaster/cast.h
+++ lib/raycaster/cast.h
@@ -9,6 +9,57 @@
 #include "colors.h"
 #include <unordered_map>
 
+void movePlayer(Player *player, float deltaX, float deltaY)
+{
+    float newX = player->x + deltaX;
+    float newY = player->y + deltaY;
+
+    // Define a standard size for the player
+    float playerSize = 0.8f;
+
+    // Check the four corners of the player
+    for (int corner = 0; corner < 4; corner++)
+    {
+        float cornerX = newX + (corner % 2 == 0 ? -playerSize : playerSize);
+        float cornerY = newY + (corner < 2 ? -playerSize : playerSize);
+
+        int i = static_cast<int>(cornerX / blockWidth);
+        int j = static_cast<int>(cornerY / blockHeight);
+
+        // Check if the proposed move would hit a wall
+        if (i >= 0 && i < mapV.size() && j >= 0 && j < mapV[i].size())
+        {
+            char mapChar = mapV.at(j).at(i); // Get the character at the new position
+
+            // If the character is 'g', display the victory screen
+            if (mapChar == 'g')
+            {
+                spr.fillSprite(TFT_BLACK);                                        // Fill screen with black color
+                spr.setTextColor(TFT_WHITE);                                      // Set text color to white
+                spr.drawString("You Win!", spr.width() / 2, spr.height() / 2, 2); // Draw "You Win!" in the center of the screen
+                spr.pushSprite(0, 0);                                             // Update the screen
+
+                delay(2000); // Wait for 2000ms
+
+                spr.fillSprite(TFT_BLACK); // Clear the screen
+
+                // Here you can restart the game or do something else
+                return;
+            }
+
+            // If the move would hit a wall, don't allow the move
+            if (mapChar != ' ')
+            {
+                return;
+            }
+        }
+    }
+
+    // If none of the corners hit a wall, allow the move
+    player->x = newX;
+    player->y = newY;
+}
+
 Impact castRay(float angle, Player *player)
 {
     float d = 0;
@@ -31,7 +82,7 @@ Impact castRay(float angle, Player *player)
         }
         miniMap.drawPixel(x, y, TFT_YELLOW);
 
-        d += 0.2f;
+        d += 0.2f; // Decrease the step size
     };
     return Impact{d, colors[hit]};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,32 @@ int valueY = 0;
 float sensibility = 0.1f;
 float sensibilityPos = 0.5f;
 float deathZone = 0.1f;
+unsigned long lastTime;
+unsigned long frameCount;
+unsigned long fps;
 
 void setup()
 {
   Serial.begin(115200);
   initScreen();
+
+  // Draw welcome screen
+  spr.fillSprite(TFT_BLACK);                                        // Fill screen with black color
+  spr.setTextColor(TFT_WHITE);                                      // Set text color to white
+  spr.drawString("Bienvenido!", spr.width() / 2 - 30, spr.height() / 2 - 10, 2); // Draw "Welcome!" in the center of the screen
+  spr.pushSprite(0, 0);                                             // Update the screen
+
+  delay(2000); // Wait for 2000ms
+
+  spr.fillSprite(TFT_BLACK); // Clear the screen
+
+  // Continue with the rest of the setup
   initBG(&spr);
   initMap(&player);
   setupSD();
+  lastTime = millis();
+  frameCount = 0;
+  fps = 0;
 }
 
 void loop()
@@ -50,11 +68,22 @@ void loop()
   {
     float deltaX = cos(player.angle) * mappedY * sensibilityPos;
     float deltaY = sin(player.angle) * mappedY * sensibilityPos;
-    player.y += deltaY;
-    player.x += deltaX;
+    movePlayer(&player, deltaX, deltaY); // Call movePlayer here
   }
 
   loopMiniMap();
   render(&player, &spr);
+
+  // Calculate FPS
+  frameCount++;
+  if (millis() - lastTime >= 1000) // Every second
+  {
+    fps = frameCount;
+    frameCount = 0;
+    lastTime = millis();
+  }
+
+  String fpsText = "FPS: " + String(fps);
+  spr.drawString(fpsText, spr.width() - 50, spr.height() - 15, 2); // Draw FPS at bottom-right corner
   spr.pushSprite(0, 0);
 }
